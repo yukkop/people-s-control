de02a2be041b622b1b6a9bb7d807532dbeda0cee
Lysmi <lysmi.mx@mail.ru>
2022-01-18 01:02:03 +0300

    Hash bugfix

---
+++ PeoplesControl/ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Logic.Auth.SaltHashToString("Max", "N"));
+
+        }
+    }
+}
---
+++ PeoplesControl/Logic/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Logic
+{
+    public class Auth
+    {
+        public static byte[] SaltHash(string value, string salt)
+        {
+            using SHA256 sha256Hash = SHA256.Create();
+            return sha256Hash.ComputeHash(Encoding.UTF8.GetBytes((value + salt)));
+        }
+
+        public static string SaltHashToString(string value, string salt)
+        {
+            using SHA256 sha256Hash = SHA256.Create();
+            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes((value + salt)));
+            var sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
