c56af4e6a78c1b51dd94c7bdc11994199b1401d9
Lysmi <lysmi.mx@mail.ru>
2022-01-21 08:15:31 +0300

    Created main menu and config project

--- Android/app/src/main/java/com/hectik/androidpeoples/MainMenuActivity.kt
+++ Android/app/src/main/java/com/hectik/androidpeoples/MainMenuActivity.kt
@@ -3,11 +3,13 @@ package com.hectik.androidpeoples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity()
+class MainMenuActivity : AppCompatActivity()
 {
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
     }
+
 }
