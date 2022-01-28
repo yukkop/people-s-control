57e352911c810ba389c0a530b543d9bd9d06d383
yukkop <yuukkop@gmail.com>
2022-01-28 11:15:40 +0300

    impliment regions

--- PeoplesControl/API/Controllers/HCS/RegionController.cs
+++ PeoplesControl/API/Controllers/HCS/RegionController.cs
@@ -4,11 +4,85 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Logic.ReadServices;
+using Logic.WriteServices;
+using Logic.WebEntities;
+using Logic.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class RegionController : Controller
     {
-        
+        IRegionReadService _regionReadService;
+        IRegionWriteService _regionWriteService;
+        IAuthorizationService _authorizationService;
+        IConfiguration _configuration;
+        public RegionController(
+            IRegionReadService regionReadService, 
+            IAuthorizationService authorizationService, 
+            IConfiguration configuration,
+            IRegionWriteService regionWriteService
+            )
+        {
+            _regionReadService = regionReadService;
+            _regionWriteService = regionWriteService;
+            _authorizationService = authorizationService;
+            _configuration = configuration;
+        }
+
+        [HttpGet("Supported")]
+        public List<SupportedRegionDTO> GetSupported([FromHeader] string Authorization)
+        {
+            if (_authorizationService.Authorization(Authorization, _configuration["ActionsConfig:Region:Supported"]))
+            {
+                return _regionReadService.GetSupported();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpGet("Unsupported")]
+        public List<UnsupportedRegionDTO> GetUnsupported([FromHeader] string Authorization)
+        {
+            if (_authorizationService.Authorization(Authorization, _configuration["ActionsConfig:Region:Unsupported"]))
+            {
+                return _regionReadService.GetUnsupported();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpPut("{id}/MakeItSupported")]
+        public bool MakeItSupported([FromHeader] string Authorization, long id)
+        {
+            if (_authorizationService.Authorization(Authorization, _configuration["ActionsConfig:Region:Unsupported"]))
+            {
+                return _regionWriteService.MakeItSupported(id);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [HttpPut("{id}/MakeItUnsupported")]
+        public bool MakeItUnsupported([FromHeader] string Authorization, long id)
+        {
+            if (_authorizationService.Authorization(Authorization, _configuration["ActionsConfig:Region:Unsupported"]))
+            {
+                return _regionWriteService.MakeItUnsupported(id);
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
---
+++ PeoplesControl/Logic/WriteServices/HCS/RegionWriteService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using DataBase.Models;
+using Logic.Repositories;
+using Logic.WebEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.WriteServices
+{
+    public class RegionWriteService : IRegionWriteService
+    {
+        IRegionRepository _regionRepository;
+        IActionMetaRepository _actionMetaRepository;
+        IMapper _mapper;
+        public RegionWriteService(IRegionRepository regionRepository, IMapper mapper, IActionMetaRepository actionMetaRepository)
+        {
+            _regionRepository = regionRepository;
+            _mapper = mapper;
+            _actionMetaRepository = actionMetaRepository;
+    }
+
+        public bool MakeItSupported(long id)
+        {
+            Region entity = _regionRepository.Get(id);
+            if (entity.IsRegionSupported != true)
+            {
+                entity.IsRegionSupported = true;
+                _regionRepository.Update(entity);
+                _regionRepository.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool MakeItUnsupported(long id)
+        {
+            Region entity = _regionRepository.Get(id);
+            if (entity.IsRegionSupported != false)
+            {
+                entity.IsRegionSupported = false;
+                _regionRepository.Update(entity);
+                _regionRepository.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Add(CreateRegionDTO createEntity, long userId)
+        {
+            Region entity = _mapper.Map<Region>(createEntity);
+
+            ActionMeta creation = new ActionMeta();
+            creation.Date = DateTime.Now;
+            _actionMetaRepository.Add(creation);
+        }
+    }
+}
